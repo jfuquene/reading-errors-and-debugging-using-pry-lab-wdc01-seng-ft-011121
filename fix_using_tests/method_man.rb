fba52660ae54f6788eaa94ef673fa8e9c4b00540
John Fuquene <jfuquene@email.wm.edu>
2020-11-11 23:18:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
-  binding.pry 
-  puts "jello"
+  "Hello #{player1} & #{player2}"
+  
 end
 
 def play_game(player2)
