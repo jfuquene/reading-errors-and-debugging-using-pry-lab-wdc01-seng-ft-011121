32a588ce11bc9e61236acd60f38c08632abcdb84
John Fuquene <jfuquene@email.wm.edu>
2020-11-11 23:13:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num = 1
-    "YUM YUM MUNCH MUNCH MUNCH"
+    p "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    p "HAM HAM HAM IN MY TUMMY"
 end
 
 def runner
